638cc51f0c51bb78d0495ccf53e7ec351cfe672d
adikagit <adikagit00@gmail.com>
2024-04-01 13:13:16 +0600

    prem

--- lib/core/bi_dc.dart
+++ lib/core/bi_dc.dart
@@ -1,6 +1,6 @@
 class DocFF {
-  static const String bsdswqw = 'app_cBsfgUkH1MsxxurAm9SmXfsLSKA9oh';
-  static const String pP = 'https://pub.dev/';
-  static const String tUse = 'https://pub.dev/';
-  static const String s = 'https://pub.dev/';
+  static const String bsdswqw = 'app_spp61JwsQg2hFRyZZKhgNmF2TSA1vc';
+  static const String pP = 'https://docs.google.com/document/d/1d1eIsG6rDrAlwkHGwESNmjKwFQRC3Go338VhTYycNfk/edit?usp=sharing';
+  static const String tUse = 'https://docs.google.com/document/d/17sfu3CwAbvbzJFp12oO5cEwJGknTkyuPZ2OUcOzGSrM/edit?usp=sharing';
+  static const String s = 'https://forms.gle/1hrxhUmvzqUPq85W9';
 }
